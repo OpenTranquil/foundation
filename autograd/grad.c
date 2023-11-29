1f69c78273db5e6a2127ff00b49808ff9dd4dbfe
neo <nerosoft@outlook.com>
2023-11-29 18:20:06 +0800

    na

--- autograd/grad.c
+++ autograd/grad.c
@@ -25,6 +25,7 @@ typedef struct OperatorFunc {
 
 typedef struct ComputeNode {
     NodeType type;
+    struct ComputeNode *parent;
 
     double grad;
     union {
@@ -52,6 +53,26 @@ double forword(ComputeNode *node) {
     return node->operator.op->forword(node);
 }
 
+double backward(ComputeNode *node) {
+    ComputeNode *cur = node;
+    while (cur != NULL) {
+        if (cur->type == VARIABLE) {
+            cur->grad = 1.0f;
+        }
+        if (cur->type == CONSTANT) {
+            printf("cannot compute grad for constant!\n");
+            exit(1);
+        }
+        if (cur->type == BINARY_OPERATOR) {
+            cur->operator.op->backward(cur);
+        }
+        if (cur->parent == NULL) {
+            return cur->grad;
+        }
+        cur = cur->parent;
+    }
+}
+
 double op_pow_forword(struct ComputeNode *node) {
     ComputeNode *left = node->operator.left;
     ComputeNode *right = node->operator.right;
@@ -59,7 +80,11 @@ double op_pow_forword(struct ComputeNode *node) {
 }
 
 double op_pow_backword(struct ComputeNode *node) {
-    return 0; //TODO
+    ComputeNode *left = node->operator.left;
+    ComputeNode *right = node->operator.right;
+    double grad = forword(right) * forword(left) * left->grad;
+    node->grad = grad;
+    return grad;
 }
 
 OperatorFunc op_pow = {
@@ -75,7 +100,10 @@ double op_add_forword(struct ComputeNode *node) {
 }
 
 double op_add_backword(struct ComputeNode *node) {
-    return 1;
+    ComputeNode *left = node->operator.left;
+    double grad = 1.0f * left->grad;
+    node->grad = grad;
+    return grad;
 }
 
 OperatorFunc op_add = {
@@ -92,7 +120,11 @@ double op_mul_forword(struct ComputeNode *node) {
 }
 
 double op_mul_backword(struct ComputeNode *node) {
-    return 0; //TODO
+    ComputeNode *right = node->operator.right;
+    ComputeNode *left = node->operator.left;
+    double grad = forword(right) * left->grad;
+    node->grad = grad;
+    return grad;
 }
 
 OperatorFunc op_mul = {
@@ -100,16 +132,6 @@ OperatorFunc op_mul = {
     .forword = op_mul_forword,
     .backward = op_mul_backword,
 };
-
-double backward(ComputeNode *node) {
-    if (node->type == VARIABLE) {
-        return 1;
-    }
-    if (node->type == CONSTANT) {
-        return 0;
-    }
-    return node->operator.op->backward(node);
-}
 
 // f = (ax + b)^2
 // Z = ax
@@ -151,30 +173,43 @@ int main(int argc, char *argv[]) {
     // variable node init
     node_2.type = CONSTANT;
     node_2.constant.val = 2.0f;
+    node_2.grad = 1.0f;
     node_a.type = CONSTANT;
     node_a.constant.val = a;
+    node_a.grad = 1.0f;
     node_x.type = VARIABLE;
     node_x.variable.val = x;
+    node_x.grad = 1.0f;
     node_b.type = CONSTANT;
     node_b.constant.val = b;
+    node_b.grad = 1.0f;
 
     // operator node init
     node_pow.type = BINARY_OPERATOR;
+    node_pow.grad = 1.0f;
     node_pow.operator.left = &node_add;
     node_pow.operator.right = &node_2;
     node_pow.operator.op = &op_pow;
 
     node_add.type = BINARY_OPERATOR;
+    node_add.grad = 1.0f;
     node_add.operator.left = &node_mul;
     node_add.operator.right = &node_b;
     node_add.operator.op = &op_add;
 
     node_mul.type = BINARY_OPERATOR;
+    node_mul.grad = 1.0f;
     node_mul.operator.left = &node_x;
     node_mul.operator.right = &node_a;
     node_mul.operator.op = &op_mul;
 
-    printf("ACTUAL val: %f\n", forword(&node_pow));
-    printf("ACTUAL grad: %f\n", backward(&node_x));
+    node_2.parent = &node_pow;
+    node_a.parent = &node_mul;
+    node_x.parent = &node_mul;
+    node_b.parent = &node_add;
+    node_add.parent = &node_pow;
+    node_mul.parent = &node_add;
+
+    printf("ACTUAL val: %f, grad:%f\n", forword(&node_pow), backward(&node_x));
     return 0;
 }
